c26c7c4ce3539fc186150ba20bfc76eb91c2d332
Nikita <vinsmir@mail.ru>
2023-04-08 14:49:55 +0300

    final fixes

--- js/big-picture.js
+++ js/big-picture.js
@@ -3,17 +3,27 @@ import {isEscapeKey} from './util.js';
 const bigPicture = document.querySelector('.big-picture');
 const cancelButton = document.querySelector('.big-picture__cancel');
 const commentList = document.querySelector('.social__comments');
-const createComment = document.querySelector('.social__text');
+const commentItem = document.querySelector('.social__comment');
 const body = document.querySelector('body');
 const commentCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.social__comments-loader');
+
+const createComment = ({avatar, name, message}) => {
+  const commentElement = commentItem.cloneNode(true);
+
+  commentElement.querySelector('.social__picture').src = avatar;
+  commentElement.querySelector('.social__picture').alt = name;
+  commentElement.querySelector('.social__text').textContent = message;
+
+  return commentElement;
+};
 
 const renderComments = (comments) => {
 
   const fragment = document.createDocumentFragment();
   comments.forEach((comment) => {
-    const commentElement = createComment(comment);
-    fragment.append(commentElement);
+    const commentText = createComment(comment);
+    fragment.append(commentText);
   });
 
   commentList.innerHTML = '';
@@ -27,7 +37,7 @@ const openBigPicture = (evt) => {
     document.body.classList.add('modal-open');
     bigPicture.querySelector('.big-picture__img img').src = evt.target.src;
   }
-  document.addEventListener('keydown');
+  document.addEventListener('keydown', onDocumentKeydown);
 };
 
 bigPicture.addEventListener('click', openBigPicture);
@@ -56,15 +66,15 @@ const renderPictureDetails = ({url, likes, description}) => {
   bigPicture.querySelector('.social__caption').textContent = description;
 };
 
-const showBigPicture = (data) => {
+const showBigPicture = (index) => {
   bigPicture.classList.remove('hidden');
   document.body.classList.add('modal-open');
   commentsLoader.classList.add('hidden');
   commentCount.classList.add('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
 
-  renderPictureDetails(data);
-  renderComments(data.comments);
+  renderPictureDetails(index);
+  renderComments(index.comments);
 };
 
 cancelButton.addEventListener('click', onCancelButtonClick);
