cc3e68191ed6caaa627cea7ec94d52d447fc5465
Nikita <vinsmir@mail.ru>
2023-04-13 23:38:34 +0300

    fix form

--- js/user-form.js
+++ js/user-form.js
@@ -1,5 +1,5 @@
 // Модуль работы формы
-import {dataForForm} from './data-form.js';
+import {DataForForm} from './data-form.js';
 import {isEscapeKey} from './util.js';
 
 const form = document.querySelector('.img-upload__form');
@@ -23,14 +23,14 @@ function validateHashtag (value) {
   return tagsList;
 }
 
-const isValidHashtag = (value) => dataForForm.VALID_SYMBOLS.test(value);
+const isValidHashtag = (value) => DataForForm.VALID_SYMBOLS.test(value);
 
 const isUniqHashtags = (value) => {
   const lowerCaseHashtags = value.map((hashtag) => hashtag.toLowerCase());
   return lowerCaseHashtags.length === new Set(lowerCaseHashtags).size;
 };
 
-const checkingForQuantityHashtags = (value) => value.length <= dataForForm.MAX_HASHTAG_QUANTITY;
+const checkingForQuantityHashtags = (value) => value.length <= DataForForm.MAX_HASHTAG_QUANTITY;
 
 const validateUniqueHashtags = (value) => {
   validateHashtag(value,tagsList);
@@ -50,19 +50,19 @@ const validateValidHashtags = (value) => {
 pristine.addValidator(
   hashtagField,
   validateQuantityHashtags,
-  dataForForm.ERROR_MESSAGE_HASHTAG_QUANTITY,
+  DataForForm.ERROR_MESSAGE_HASHTAG_QUANTITY,
 );
 
 pristine.addValidator(
   hashtagField,
   validateUniqueHashtags,
-  dataForForm.ERROR_MESSAGE_UNIQUE_HASHTAG,
+  DataForForm.ERROR_MESSAGE_UNIQUE_HASHTAG,
 );
 
 pristine.addValidator(
   hashtagField,
   validateValidHashtags,
-  dataForForm.ERROR_MESSAGE_VALID_HASHTAG,
+  DataForForm.ERROR_MESSAGE_VALID_HASHTAG,
 );
 
 const isFieldFocus = () =>
@@ -97,14 +97,11 @@ const onCancelButtonClick = () => {
   modalClose();
 };
 
-const submitForm = () => {
-  form.addEventListener('submit', async (evt) => {
+const submitForm = (evt) => {
+  const isValid = pristine.validate();
+  if (!isValid) {
     evt.preventDefault();
-    const isValid = pristine.validate();
-    if (!isValid) {
-      evt.preventDefault();
-    }
-  });
+  }
 };
 
 uploadStart.addEventListener('change', onUploadFileChange);
